2d202662c2d2a0c72e94759ebbea07bdb8bbaa71
adrien.dumazaud <adrien.dumazaud@epita.fr>
2025-10-23 11:38:30 +0200

    get env for portability

--- home.nix
+++ home.nix
@@ -6,8 +6,8 @@
     ];
 # Home Manager needs a bit of information about you and the paths it should
 # manage.
-    home.username = "adriendumazaud";
-    home.homeDirectory = "/Users/adriendumazaud";
+    home.username = builtins.getEnv "USER";
+    home.homeDirectory = builtins.getEnv "HOME";
 
 # This value determines the Home Manager release that your configuration is
 # compatible with. This helps avoid breakage when a new Home Manager release
